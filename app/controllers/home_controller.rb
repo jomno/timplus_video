ae7a2b11111b93ebe085f78f4c2abd4f0d7f291f
nojongwon <jake8955@naver.com>
2017-08-11 04:49:27 +0000

    dd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,20 @@
 class HomeController < ApplicationController
-  before_action :set_category, only: [:show]
+  before_action :set_category,:sort, only: [:show]
   def index
     @curriculums=Curriculum.all
     @title = "전체 커리큘럼 보기"
+    
   end
   def show
-    @curriculums=@category.curriculums
+    # @curriculums=@category.curriculums
     @title = @category.name+" 커리큘럼 보기"
-    render 'index'
+    # render 'index'
+    if params[:sort]
+      respond_to do |format|
+        format.js
+      end
+    end
+    
   end
   def create
     @category=Category.new(category_params)
@@ -26,5 +33,19 @@ class HomeController < ApplicationController
   end
   def category_params
     params.require(:category).permit(:name,:description)
+  end
+  def sort
+    set_category
+    if params[:sort]
+      case params[:sort].to_i
+        when 1 then @curriculums=@category.curriculums # 전체
+        when 2 then @curriculums=@category.curriculums # 무료
+        when 3 then @curriculums=@category.curriculums # 유료
+        when 4 then @curriculums=@category.curriculums.where(user_id: current_user) # 나의 강의
+        when 5 then @curriculums=@category.curriculums # 수강 강의
+      end
+    else
+      @curriculums=@category.curriculums
+    end
   end
 end
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -7,6 +7,6 @@ CarrierWave.configure do |config|
     region:                'ap-northeast-2'                # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'timplevideo'                          # required
-  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_public     = true                                        # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
